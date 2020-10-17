e0e965c00cb3e9b07c29f7bae84520110226767e
ekem159 <ekem159@naver.com>
2020-10-17 17:43:37 +0900

    calculator complete

--- homework-6/main.js
+++ homework-6/main.js
@@ -1,86 +1,80 @@
-const cal = document.querySelector('.cal');
-const input = document.querySelector('.cal__input');
-const NUMBER_BTN_CLASS = 'number__btn';
-const operatorBtn = ['+', '-', '*', '/'];
-const RESET = 'C';
-const EQUAL_BTN = '=';
-let numArray = [];
-let OPERATOR = [];
-let getArg = [];
+const result = document.querySelector(".js-result");
+const reset = document.querySelector(".js-reset");
+const equals = document.querySelector(".js-equals");
+const numbers = Array.from(document.querySelectorAll(".js-number"));
+const operations = Array.from(document.querySelectorAll(".js-operation"));
 
-function paintText(text) {
-    if (text !== '') input.value = `${text}`;
-}
-function addOperation() {
-    const sum = parseInt(getArg.shift()) + parseInt(getArg.shift());
-    paintText(sum);
-}
-function minusOperation() {
-    const minus = parseInt(getArg.shift()) - parseInt(getArg.shift());
-    paintText(minus);
-}
-function multipleOperation() {
-    const multiple = parseInt(getArg.shift()) * parseInt(getArg.shift());
-    paintText(multiple);
-}
-function dividedOperation() {
-    const divided = parseInt(getArg.shift()) / parseInt(getArg.shift());
-    paintText(divided);
-}
-function mathOperation(currentOperator) {
-    switch (currentOperator) {
-        case operatorBtn[0]: addOperation();
-            break;
-        case operatorBtn[1]: minusOperation();
-            break;
-        case operatorBtn[2]: multipleOperation();
-            break;
-        case operatorBtn[3]: dividedOperation();
-            break;
-    }
-}
-function searchOpeator(text) {
-    const mathOperator = operatorBtn.filter(item => text === item);
-    return mathOperator.toString();
-}
-function getValue() {
-    cal.addEventListener('click', event => {
-        if (event.target.tagName === 'DIV') return;
-        const value = event.target.textContent;
-        if (event.target.classList.contains(NUMBER_BTN_CLASS)) {
-            setArg(value);
+let firstValue = "",
+    firstDone,
+    secondValue = "",
+    secondDone,
+    currentOperation;
+
+function doOperation() {
+    const intValueA = parseInt(firstValue, 10);
+    const intValueB = parseInt(secondValue, 10);
+    switch (currentOperation) {
+        case "+":
+            return intValueA + intValueB;
+        case "-":
+            return intValueA - intValueB;
+        case "/":
+            return intValueA / intValueB;
+        case "*":
+            return intValueA * intValueB;
+        default:
             return;
-        }
-        if (value !== EQUAL_BTN) { //연산자 버튼 클릭 시
-            getArg.push(input.value);
-            initArray();
-            OPERATOR.push(searchOpeator(value));
-            if (value === RESET) {
-                initArray();
-                paintText(0);
-                return;
-            }
-        }
-        else { // 이콜버튼 
-            getArg.push(input.value);
-            mathOperation(OPERATOR.forEach(item=>item));
-        };
-    });
-}
-function setArg(text) {
-    const numObj = {
-        text
     }
-    numArray.push(numObj);
-    const arrayElement = numArray.map(item => item.text);
-    const joinElement = parseInt(arrayElement.join(''));
-    paintText(joinElement);
 }
-function initArray() {
-    numArray.splice(0, numArray.length);
+function handleNumberClick(e) {
+    const clickedNum = e.target.innerText;
+    if (!firstDone) {
+        firstValue = firstValue + clickedNum;
+        result.innerHTML = firstValue;
+    } else {
+        secondValue = secondValue + clickedNum;
+        result.innerHTML = secondValue;
+        secondDone = true;
+    }
 }
-function init() {
-    paintText(0);
-    getValue();
+function calculate() {
+    const operation = doOperation();
+    result.innerHTML = operation;
+    firstValue = operation;
+    secondDone = false;
+    secondValue = "";
 }
-init();
+
+function handleOperationClick(e) {
+    const clickedOperation = e.target.innerText;
+    if (!firstDone) { //첫번째 연산을 끝냈다면 true라서 false가 된다 조건 패스 . 
+        firstDone = true;
+    }
+    if (firstDone && secondDone) {
+        calculate();
+    }
+    currentOperation = clickedOperation;
+}
+
+function handleReset() {
+    firstValue = "";
+    secondValue = "";
+    firstDone = false;
+    secondDone = false;
+    currentOperation = null;
+    result.innerHTML = "0";
+}
+
+function handleEqualsClick() {
+    if (firstDone && secondDone) {
+        calculate();
+    }
+}
+numbers.forEach(function(number) {
+    number.addEventListener("click", handleNumberClick);
+});
+operations.forEach(function (operation) {
+    operation.addEventListener("click", handleOperationClick);
+});
+reset.addEventListener("click", handleReset);
+equals.addEventListener("click", handleEqualsClick);
